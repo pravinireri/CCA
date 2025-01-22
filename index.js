042e724f4f1033a7150762ceaf74acfd038be993
pravinireri <pkireri@gmail.com>
2025-01-22 18:32:11 +0300

    added preloader

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* MENU */
+
 const menuButton = document.getElementById('menu-button');
 const closeButton = document.getElementById('close-button');
 const overlay = document.getElementById('overlay');
@@ -10,4 +12,18 @@ menuButton.addEventListener('click', () => {
 // Hide overlay
 closeButton.addEventListener('click', () => {
   overlay.style.display = 'none';
+});
+
+
+/* PRELOADER */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const preloader = document.getElementById("preloader");
+  const mainContent = document.getElementById("main-content");
+
+  // Simulated Delay
+  setTimeout(() => {
+    preloader.classList.add("hidden"); // fade-out effect
+    mainContent.classList.remove("hidden"); // reveals main content
+  }, 1600); // delay
 });
